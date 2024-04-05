daff684ce881d8f6017a5413aa656aa0598f6be5
Ben <behnam.iam@gmail.com>
2024-04-06 00:23:51 +0100

    wpf and status endpoint

---
+++ src/AzureFunctions/HttpTrigger/Status.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctions.HttpTrigger;
+
+public class Status
+{
+    private readonly ILogger _logger;
+
+    public Status(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<Status>();
+    }
+
+    [Function("Status")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+        FunctionContext executionContext)
+    {
+        var I = new HttpResponseData().StatusCode = ;
+        return HttpResponseData.CreateResponse() ;
+    }
+}
